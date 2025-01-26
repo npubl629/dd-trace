0fffefe91d593dafc3956d5ce2f71e4a39061551
npubl629 <npubl629>
2025-01-26 08:19:14 +0000

    final postprocessing charts

--- postprocessing/generate_images.py
+++ postprocessing/generate_images.py
@@ -6,46 +6,69 @@ import matplotlib.pyplot as plt
 def get_data():
     start_time = datetime.datetime(2024, 7, 17, 18)
     end_time = start_time + datetime.timedelta(days=7)
-    now = datetime.datetime.now()
+    now = datetime.datetime(2025, 1, 21, 18)
 
     headers = {
         'DD-API-KEY': os.environ['DD-API-KEY'],
         'DD-APPLICATION-KEY': os.environ['DD-APPLICATION-KEY']
     }
 
-    pointlists_by_container = {}
+    cpu_pointlists_by_container = {}
+    mem_pointlists_by_service = {}
 
     while start_time <= now:
-        batch_params = {
+        cpu_batch_params = {
             'from': start_time.timestamp(),
             'to': end_time.timestamp(),
             'query': 'avg:container.cpu.usage{container_name:trace-metrics-healthcheck-1 OR container_name:trace-nometrics-healthcheck-1 OR container_name:trace-control-healthcheck-1} by {container_name}'
         }
 
-        print(f'Querying batch, {batch_params}')
+        print(f'Querying batch, {cpu_batch_params}')
 
-        json_batch = requests.get('https://api.datadoghq.com/api/v1/query', params=batch_params, headers=headers).json()
+        cpu_json_batch = requests.get('https://api.datadoghq.com/api/v1/query', params=cpu_batch_params, headers=headers).json()
+        cpu_series = cpu_json_batch['series']
+        for i in range(len(cpu_series)):
+            name = cpu_series[i]['tag_set'][0][len('container_name:'):]
+            pointlist = cpu_series[i]['pointlist']
+            if not name in cpu_pointlists_by_container:
+                cpu_pointlists_by_container[name] = []
+            cpu_pointlists_by_container[name] += pointlist
 
-        series = json_batch['series']
-        for i in range(len(series)):
-            name = series[i]['tag_set'][0][len('container_name:'):]
-            pointlist = series[i]['pointlist']
-            if not name in pointlists_by_container:
-                pointlists_by_container[name] = []
-            pointlists_by_container[name] += pointlist
+        mem_batch_params = {
+            'from': start_time.timestamp(),
+            'to': end_time.timestamp(),
+            'query': 'ewma_5(avg:runtime.dotnet.mem.committed{service:metrics-healthcheck OR service:metrics-idle} by {service})'
+        }
+
+        mem_json_batch = requests.get('https://api.datadoghq.com/api/v1/query', params=mem_batch_params, headers=headers).json()
+        mem_series = mem_json_batch['series']
+        for i in range(len(mem_series)):
+            name = mem_series[i]['tag_set'][0][len('service:'):]
+            pointlist = mem_series[i]['pointlist']
+            if not name in mem_pointlists_by_service:
+                mem_pointlists_by_service[name] = []
+            mem_pointlists_by_service[name] += pointlist
 
         start_time = end_time
         end_time = end_time + datetime.timedelta(days=7)
 
-    data_by_container = {}
-    for container_name in pointlists_by_container:
-        values = pointlists_by_container[container_name]
+    cpu_data_by_container = {}
+    for container_name in cpu_pointlists_by_container:
+        values = cpu_pointlists_by_container[container_name]
         cpu_values = [item[1] / 1_000_000 for item in values] # convert microcores to millicores
         timestamps = [item[0] / 1000 for item in values] # convert ms since epoch to seconds since epoch
         dates = [datetime.datetime.fromtimestamp(ts) for ts in timestamps]
-        data_by_container[container_name] = {'dates': dates, 'cpu_values': cpu_values}
+        cpu_data_by_container[container_name] = {'dates': dates, 'cpu_values': cpu_values}
 
-    return data_by_container
+    mem_data_by_service = {}
+    for service in mem_pointlists_by_service:
+        values = mem_pointlists_by_service[service]
+        mem_values = [item[1] / (1024*1024) for item in values] # convert to MiB
+        timestamps = [item[0] / 1000 for item in values] # convert ms since epoch to seconds since epoch
+        dates = [datetime.datetime.fromtimestamp(ts) for ts in timestamps]
+        mem_data_by_service[service] = {'dates': dates, 'mem_values': mem_values}
+
+    return cpu_data_by_container, mem_data_by_service
 
 
 def generate_cpu_chart(data_by_container):
@@ -93,11 +116,32 @@ def generate_cpu_diff_chart(data_by_container):
     ax.legend()
     return fig
 
-if '__main__' == __name__:
-    data_by_container = get_data()
+def generate_mem_chart(mem_data):
+    fig = plt.figure(figsize=(15, 5))
+    fig.tight_layout(pad=3)
+    ax = fig.add_subplot()
+    ax.set_xlabel('Date')
+    ax.set_ylabel('.Net Committed Memory Value (MiB)')
+    ax.grid(True)
+    ax.set_title(f'.Net Committed Memory by Service')
 
-    fig1 = generate_cpu_chart(data_by_container)
-    fig2 = generate_cpu_diff_chart(data_by_container)
+    idle = mem_data['metrics-idle']['mem_values']
+    healthcheck = mem_data['metrics-healthcheck']['mem_values']
+    dates = mem_data['metrics-idle']['dates']
+
+    ax.plot(dates, idle, label='metrics-idle')
+    ax.plot(dates, healthcheck, label='metrics-healthcheck')
+    ax.legend()
+
+    return fig
+
+if '__main__' == __name__:
+    cpu_data, mem_data = get_data()
+
+    fig1 = generate_cpu_chart(cpu_data)
+    fig2 = generate_cpu_diff_chart(cpu_data)
+    fig3 = generate_mem_chart(mem_data)
 
     fig1.savefig('cpu-usage.png')
     fig2.savefig('cpu-diff.png')
+    fig3.savefig('mem-diff.png')
